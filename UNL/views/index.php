a371642d7f077f4913271a1a83f1ffb3e13449db
Seth Meranda <smeranda2@unl.edu>
2009-03-08 23:57:18 +0000

    created XHTML for the form

--- UNL/views/index.php
+++ UNL/views/index.php
@@ -1,3 +1,45 @@
-<?php
-echo 'This is a test';
-?>
+<h1 class="sec_main">Create a shorter URL</h1>
+<fieldset>
+	<legend>Basic Option</legend>
+	<ol>
+		<li>
+			<label for="theURL">Long URL<em>*</em></label>
+			<input name="theURL" id="theURL" type="text" />
+		</li>
+	</ol>
+</fieldset>
+<fieldset>
+	<legend>Custom Alias</legend>
+	<ol>
+		<li>
+			<label for="theAlias">Alias</label>
+			<input name="theAlias" id="theAlias" type="text" />
+		</li>
+	</ol>
+</fieldset>
+<fieldset>
+	<legend>Google Analytics Campaign Tagging</legend>
+	<ol>
+		<li>
+			<label for="gaSource">Source</label>
+			<input name="gaSource" id="gaSource" type="text" />
+		</li>	
+		<li>
+			<label for="gaMedium">Medium</label>
+			<input name="gaMedium" id="gaMedium" type="text" />
+		</li>
+		<li>
+			<label for="gaTerm">Term</label>
+			<input name="gaTerm" id="gaTerm" type="text" />
+		</li>
+		<li>
+			<label for="gaContent">Content</label>
+			<input name="gaContent" id="gaContent" type="text" />
+		</li>
+		<li>
+			<label for="gaName">Name</label>
+			<input name="gaName" id="gaName" type="text" />
+		</li>		
+	</ol>
+</fieldset>
+<p class="submit"><a id="submit" href="#">Create URL</a></p>
