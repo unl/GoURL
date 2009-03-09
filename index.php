63888b03e4df8c9abccebe1061d5feaae1e1787d
Seth Meranda <smeranda2@unl.edu>
2009-03-09 00:25:29 +0000

    Added link to css file

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ $page = UNL_Templates::factory('Fixed');
 $page->titlegraphic = "<h1>Go URL</h1><h2>The shorter the better</h2>";
 $page->doctitle = '<title>UNL | Go URL, a short URL service</title>';
 $page->leftRandomPromo = '';
-$page->head ="<script type=\"text/javascript\" src=\"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js\"></script> <script type=\"text/javascript\"> var navl2Links = 0; //Default navline2 links to display (zero based counting) </script>";
+$page->head ="<script type=\"text/javascript\" src=\"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js\"></script> <script type=\"text/javascript\"> var navl2Links = 0; //Default navline2 links to display (zero based counting) </script><link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"sharedcode/css/forms/maincontent.css\" />
+";
 ob_start();
 include 'UNL/views/index.php';
 $page->maincontentarea = ob_get_clean();
