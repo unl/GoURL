9a2227600ea6bb255f8f4244eeee4d87685dccf4
Seth Meranda <smeranda2@unl.edu>
2009-03-31 19:27:02 +0000

    Changed JS/CSS file order.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $page = UNL_Templates::factory('Fixed');
 $page->titlegraphic = "<h1>Go URL</h1><h2>The shorter the better</h2>";
 $page->doctitle = '<title>UNL | Go URL, a short URL service</title>';
 $page->leftRandomPromo = '';
-$page->head ="<script type=\"text/javascript\" src=\"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js\"></script> <script type=\"text/javascript\"> var navl2Links = 0; //Default navline2 links to display (zero based counting) </script><link rel=\"stylesheet\" type=\"text/css\"  href=\"sharedcode/css/forms/maincontent.css\" />
+$page->head ="<script type=\"text/javascript\" src=\"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js\"></script> <link rel=\"stylesheet\" type=\"text/css\"  href=\"sharedcode/css/forms/maincontent.css\" /> 
 <link rel=\"stylesheet\" type=\"text/css\" href=\"/ucomm/templatedependents/templatecss/components/forms.css\" />";
 ob_start();
 include 'UNL/views/index.php';
