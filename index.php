7cd13873be111ad1401e386b61beb668c88b5134
Seth Meranda <smeranda2@unl.edu>
2009-03-31 18:49:05 +0000

    Add reference to form CSS.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $page->titlegraphic = "<h1>Go URL</h1><h2>The shorter the better</h2>";
 $page->doctitle = '<title>UNL | Go URL, a short URL service</title>';
 $page->leftRandomPromo = '';
 $page->head ="<script type=\"text/javascript\" src=\"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js\"></script> <script type=\"text/javascript\"> var navl2Links = 0; //Default navline2 links to display (zero based counting) </script><link rel=\"stylesheet\" type=\"text/css\"  href=\"sharedcode/css/forms/maincontent.css\" />
-";
+<link rel=\"stylesheet\" type=\"text/css\" href=\"/ucomm/templatedependents/templatecss/components/forms.css\" />";
 ob_start();
 include 'UNL/views/index.php';
 $page->maincontentarea = ob_get_clean();
