a1de04a8d539c4c64b47ab169e39f84e1e5ff793
Seth Meranda <smeranda2@unl.edu>
2009-04-01 21:01:24 +0000

    PHP code added.

--- UNL/views/index.php
+++ UNL/views/index.php
@@ -1,3 +1,96 @@
+<?php /* index.php ( lilURL implementation ) */
+
+require_once 'includes/conf.php'; // <- site-specific settings
+require_once 'includes/lilurl.php'; // <- lilURL class file
+
+$lilurl = new lilURL();
+$msg = '';
+
+// if the form has been submitted
+if ( isset($_POST['theURL']) )
+{
+	// escape bad characters from the user's url
+	$longurl = trim(mysql_escape_string($_POST['theURL']));
+
+	// set the protocol to not ok by default
+	$protocol_ok = false;
+	
+	// if there's a list of allowed protocols, 
+	// check to make sure that the user's url uses one of them
+	if ( count($allowed_protocols) )
+	{
+		foreach ( $allowed_protocols as $ap )
+		{
+			if ( strtolower(substr($longurl, 0, strlen($ap))) == strtolower($ap) )
+			{
+				$protocol_ok = true;
+				break;
+			}
+		}
+	}
+	else // if there's no protocol list, screw all that
+	{
+		$protocol_ok = true;
+	}
+		
+	// add the url to the database
+	if ( $protocol_ok && $lilurl->add_url($longurl) )
+	{
+		if ( REWRITE ) // mod_rewrite style link
+		{
+			$url = 'http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']).'/'.$lilurl->get_id($longurl);
+		}
+		else // regular GET style link
+		{
+			$url = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'].'?id='.$lilurl->get_id($longurl);
+		}
+
+		$msg = '<p class="success">Your lil&#180; URL is: <a href="'.$url.'">'.$url.'</a></p>';
+	}
+	elseif ( !$protocol_ok )
+	{
+		$msg = '<p class="error">Invalid protocol!</p>';
+	}
+	else
+	{
+		$msg = '<p class="error">Creation of your lil&#180; URL failed for some reason.</p>';
+	}
+}
+else // if the form hasn't been submitted, look for an id to redirect to
+{
+	if ( isSet($_GET['id']) ) // check GET first
+	{
+		$id = mysql_escape_string($_GET['id']);
+	}
+	elseif ( REWRITE ) // check the URI if we're using mod_rewrite
+	{
+		$explodo = explode('/', $_SERVER['REQUEST_URI']);
+		$id = mysql_escape_string($explodo[count($explodo)-1]);
+	}
+	else // otherwise, just make it empty
+	{
+		$id = '';
+	}
+	
+	// if the id isn't empty and it's not this file, redirect to it's url
+	if ( $id != '' && $id != basename($_SERVER['PHP_SELF']) )
+	{
+		$location = $lilurl->get_url($id);
+		
+		if ( $location != -1 )
+		{
+			header('Location: '.$location);
+		}
+		else
+		{
+			$msg = '<p class="error">Sorry, but that lil&#180; URL isn\'t in our database.</p>';
+		}
+	}
+}
+
+// print the form
+
+?>
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function () {
 	$('.hint').hide();
@@ -7,7 +100,8 @@ $(document).ready(function () {
 	});
 });
 </script>
-<form action="#" method="POST">
+<p><?php echo $msg; ?></p>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
 <p class="required">Indicates a required field.</p>
 <fieldset>
 	<legend>Basic Option</legend>
@@ -27,7 +121,6 @@ $(document).ready(function () {
 		<li>
 			<label for="theAlias" class="element">Alias</label>
 			<div class="element"><input name="theAlias" id="theAlias" type="text" />			
-			<span class="hint"><span class="hintPointer"></span>ex: go.unl.edu/<strong>admissions</strong></span>
 			</div>
 		</li>
 	</ol>
@@ -36,13 +129,13 @@ $(document).ready(function () {
 	<legend>Google Analytics Campaign Tagging</legend>
 	<p>Add your campaign information here and it will be automatically added to your URL when redirected.</p>
 	<ol>
-		<li class="required">
+		<li>
 			<label for="gaSource" class="element">Source</label>
 			<div class="element"><input name="gaSource" id="gaSource" type="text" />			
 			<span class="hint"><span class="hintPointer"></span>The Google Analytics Source The Google Analytics Source The Google Analytics Source The Google Analytics Source The Google Analytics Source</span>
 			</div>
 		</li>	
-		<li class="required">
+		<li>
 			<label for="gaMedium" class="element">Medium</label>
 			<div class="element"><input name="gaMedium" id="gaMedium" type="text" />			
 			<span class="hint"><span class="hintPointer"></span>Testing. Testing. Testing. Testing. Testing. Testing.</span>
@@ -64,5 +157,5 @@ $(document).ready(function () {
 		<div class="element"><input name="helpful" value="1" type="radio" id="cheezyes" /><label for="cheezyes">Yes</label><input name="helpful" value="0" type="radio" id="cheezno" /><label for="cheezno">No</label></div></li>	
 	</ol>
 </fieldset>
+<p class="submit"><input type="submit" id="submit" name="submit" value="Create URL" /></p>
 </form>
-<p class="submit"><input type="submit" value="Create URL" /></p>
