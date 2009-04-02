9161c96fac713153b534c81e517bc3d539bfc1e6
Brett Bieber <brett.bieber@gmail.com>
2009-04-02 18:24:05 +0000

    Use methods for adding stylesheets and scripts to the head.

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $page = UNL_Templates::factory('Fixed');
 $page->titlegraphic = "<h1>Go URL</h1><h2>The shorter the better</h2>";
 $page->doctitle = '<title>UNL | Go URL, a short URL service</title>';
 $page->leftRandomPromo = '';
-$page->head ="<script type=\"text/javascript\" src=\"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js\"></script>  
-<link rel=\"stylesheet\" type=\"text/css\" href=\"/ucomm/templatedependents/templatecss/components/forms.css\" />
-<link rel=\"stylesheet\" type=\"text/css\"  href=\"sharedcode/css/forms/maincontent.css\" />";
+$page->addScript('http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js');
+$page->addStylesheet('/ucomm/templatedependents/templatecss/components/forms.css');
+$page->addStylesheet('sharedcode/css/forms/maincontent.css');
 ob_start();
 include 'UNL/views/index.php';
 $page->maincontentarea = ob_get_clean();
