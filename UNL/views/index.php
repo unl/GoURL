8af238477101cebf68af9d8319f52af1e8e95f5c
Seth Meranda <smeranda2@unl.edu>
2009-04-07 18:34:40 +0000

    Updated JS to indicate conditional required fields based on GA fields filled.

--- UNL/views/index.php
+++ UNL/views/index.php
@@ -94,9 +94,16 @@ else // if the form hasn't been submitted, look for an id to redirect to
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function () {
 	$('.hint').hide();
-	$('input').focus( function() {
+	$('input').focus(function() {
 		$('.hint').hide();
 		$(this).siblings('.hint').show();
+	});
+	$('#gaSource'|'#gaMedium'|'#gaName').change(function() {
+		if ($(this) != "") {
+			$('#gaSource').parent('.element').parent('li').addClass('required');
+			$('#gaMedium').parent('.element').parent('li').addClass('required');
+			$('#gaName').parent('.element').parent('li').addClass('required');
+		}
 	});
 });
 </script>
@@ -108,21 +115,22 @@ $(document).ready(function () {
 	<ol>
 		<li class="required">
 			<label for="theURL" class="element">Long URL</label>
-			<div class="element"><input name="theURL" id="theURL" type="text" />			
-			<span class="hint"><span class="hintPointer">&nbsp;</span>ex: go.unl.edu/<strong>admissions</strong></span>
+			<div class="element"><input name="theURL" id="theURL" type="text" />
 			</div>
 		</li>
 	</ol>
 </fieldset>
-
+<p class="submit"><input type="submit" id="submit1" name="submit" value="Create URL" /></p>
+<p class="clear">This is a test.</p>
 <fieldset>
 	<legend>Custom Alias</legend>
-	<p>If you would like to control the URL, then use enter the alias you would like to use.</p>
+	<p>If you would like to control the URL, then enter the alias you would like to use.</p>
 	<?php if ($cas_client->isLoggedIn()) : ?>
 	<ol>
 		<li>
 			<label for="theAlias" class="element">Alias</label>
 			<div class="element"><input name="theAlias" id="theAlias" type="text" />			
+			<span class="hint"><span class="hintPointer">&nbsp;</span>ex: go.unl.edu/<strong>admissions</strong></span>			
 			</div>
 		</li>
 	</ol>
@@ -144,29 +152,33 @@ $(document).ready(function () {
 		<li>
 			<label for="gaSource" class="element">Source</label>
 			<div class="element"><input name="gaSource" id="gaSource" type="text" />			
-			<span class="hint"><span class="hintPointer">&nbsp;</span>The Google Analytics Source The Google Analytics Source The Google Analytics Source The Google Analytics Source The Google Analytics Source</span>
+				<span class="hint"><span class="hintPointer">&nbsp;</span>referrer: google, facebook, twitter</span>
 			</div>
 		</li>	
 		<li>
 			<label for="gaMedium" class="element">Medium</label>
 			<div class="element"><input name="gaMedium" id="gaMedium" type="text" />			
-			<span class="hint"><span class="hintPointer">&nbsp;</span>Testing. Testing. Testing. Testing. Testing. Testing.</span>
+				<span class="hint"><span class="hintPointer">&nbsp;</span>marketing medium: email, web, banner</span>
 			</div>
 		</li>
 		<li>
 			<label for="gaTerm" class="element">Term</label>
-			<div class="element"><input name="gaTerm" id="gaTerm" type="text" /></div>
+			<div class="element"><input name="gaTerm" id="gaTerm" type="text" />		
+				<span class="hint"><span class="hintPointer">&nbsp;</span>identify the keywords</span>
+			</div>
 		</li>
 		<li>
 			<label for="gaContent" class="element">Content</label>
-			<div class="element"><input name="gaContent" id="gaContent" type="text" /></div>
+			<div class="element"><input name="gaContent" id="gaContent" type="text" />			
+				<span class="hint"><span class="hintPointer">&nbsp;</span>use to differentiate ads (A/B testing)</span>
+				</div>
 		</li>
 		<li>
 			<label for="gaName" class="element">Name</label>
-			<div class="element"><input name="gaName" id="gaName" type="text" /></div>
+			<div class="element"><input name="gaName" id="gaName" type="text" />			
+				<span class="hint"><span class="hintPointer">&nbsp;</span>product, promo code, or slogan</span>
+			</div>
 		</li>	
-		<li><label class="element">I Can Has Cheezburger?</label>
-		<div class="element"><input name="helpful" value="1" type="radio" id="cheezyes" /><label for="cheezyes">Yes</label><input name="helpful" value="0" type="radio" id="cheezno" /><label for="cheezno">No</label></div></li>	
 	</ol>
 </fieldset>
 <p class="submit"><input type="submit" id="submit" name="submit" value="Create URL" /></p>
