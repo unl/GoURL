33bf9e553ffc27e1af86ba8e8c1c97598204337b
Seth Meranda <smeranda2@unl.edu>
2009-10-28 21:55:25 +0000

    added CORS support.

--- api_create.php
+++ api_create.php
@@ -1,5 +1,28 @@
 <?php /* api_create.php ( accept GET and spit out a goURL ) */
 
+// Specify domains from which requests are allowed
+header('Access-Control-Allow-Origin: *');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+// The X-Requested-With header allows jQuery requests to go through
+header('Access-Control-Allow-Headers: X-Requested-With');
+
+// Exit early so the page isnÕt fully loaded for options requests
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+exit();
+}
+/*
+echo 'Hello CORS, this is '
+. $_SERVER['SERVER_NAME'] . PHP_EOL
+.'You sent a '.$_SERVER['REQUEST_METHOD'] . ' request.' . PHP_EOL;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+echo 'Your name is ' . htmlentities($_POST['name']);
+}
+*/
 require_once 'includes/conf.php'; // <- bring in the config
 require_once 'UNL/Auth.php';
 
