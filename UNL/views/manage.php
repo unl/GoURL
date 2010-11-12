d1624ade564a077065baee3bd82e3eca9e86de1a
Seth Meranda <smeranda2@unl.edu>
2010-11-12 15:56:57 +0000

    Add QR codes to the manager table.

--- UNL/views/manage.php
+++ UNL/views/manage.php
@@ -1,3 +1,8 @@
+<script type="text/javascript" charset="utf-8">
+WDN.jQuery(document).ready(function () {
+	WDN.jQuery('.qrImage').colorbox({photo:true});
+});
+</script>
 <?php
 if (isset($_POST, $_POST['urlID'])) {
     $lilurl->deleteURL($_POST['urlID'], $cas_client->getUser());
@@ -6,9 +11,10 @@ $urls = $lilurl->getUserURLs($cas_client->getUser());
 if (mysql_num_rows($urls)) {
     echo '<h3>Here you can manage the urls you\'ve submitted:</h3>';
     echo '<table class="zentable cool">
-    <tr><th>Short ID</th><th>Long URL</th><th>Delete</th></tr>';
+    <tr><th>QR Code</th><th>Short ID</th><th>Long URL</th><th>Delete</th></tr>';
     while ($row = mysql_fetch_assoc($urls)) { ?>
     <tr>
+    	<td><a class="qrImage" href="<?php echo $row['urlID']; ?>.qr"></a></td>
         <td><?php echo $row['urlID']; ?></td>
         <td><?php echo $row['longURL']; ?></td>
         <td>
