20dc72d42524e98fe02f7c125c493a4d2e2d1289
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-14 14:49:09 -0500

    Organize index and prep to use 4.0 UNL Template.

--- www/index.php
+++ www/index.php
@@ -1,8 +1,9 @@
 <?php
 require_once __DIR__ . '/../config.inc.php'; // <- site-specific settings
-
+require_once __DIR__ . '/../src/lilURL.php';
 require_once 'UNL/Auth.php';
-
+require_once 'UNL/Templates.php';
+require_once 'UNL/Templates/CachingService/Null.php';
 
 $cas_client = UNL_Auth::factory('SimpleCAS');
 if (isset($_GET['login'])) {
@@ -13,54 +14,37 @@ if (isset($_GET['logout'])) {
     $cas_client->logout();
 }
 
-$login_link = '<a href="?login">Login</a>';
-$template = 'Document';
-if ($cas_client->isLoggedIn()) {
-    $template = 'Fixed';
-    $login_link = '<a href="?logout">Logout</a>';
-}
-
-require_once 'UNL/Templates.php';
-require_once 'UNL/Templates/CachingService/Null.php';
 UNL_Templates::setCachingService(new UNL_Templates_CachingService_Null());
-UNL_Templates::$options['version'] = 3.1;
-$page = UNL_Templates::factory('Fixed');
+UNL_Templates::$options['version'] = 4;
+$page = UNL_Templates::factory('Local');
+$page->__params['class']['value'] = 'terminal';
 $page->titlegraphic = "Go URL";
 $page->pagetitle = '';
 $page->doctitle = '<title>Go URL, a short URL service | University of Nebraska-Lincoln</title>';
-$page->addStylesheet('/wdn/templates_3.1/css/content/zenform.css');
-$page->addStylesheet('/wdn/templates_3.1/css/content/notice.css');
 $page->addStylesheet('sharedcode/css/identity/serviceIndicator.css');
-
-
-$page->breadcrumbs = '<ul>
-                        <li><a href="http://www.unl.edu/">UNL</a></li>
-                        <li>Go URL</li>
-                      </ul>';
-
-require_once __DIR__ . '/../src/lilURL.php'; // <- lilURL class file
-$lilurl = new lilURL();
-$lilurl->setAllowedProtocols($allowed_protocols);
+$page->addHeadLink('./', 'home');
+$page->footercontent = '© ' . date('Y') . ' University of Nebraska-Lincoln · Lincoln, NE 68588';
+$page->addScriptDeclaration(<<<EOD
+WDN.setPluginParam('idm', 'login', './?login');
+WDN.setPluginParam('idm', 'logout', './?logout');
+EOD
+);
 
 ob_start();
-if ($cas_client->isLoggedIn()
-    && isset($_GET['manage'])) {
+if ($cas_client->isLoggedIn() && isset($_GET['manage'])) {
+    $lilurl = new lilURL();
+    $didDelete = false;
+    if (isset($_POST, $_POST['urlID'])) {
+        $lilurl->deleteURL($_POST['urlID'], $cas_client->getUser());
+        $didDelete = true;
+    }
     // Show the url management screen
     include __DIR__ . '/templates/manage.php';
 } else {
     // Show the submission interface
+    include  __DIR__ . '/../src/action.php';
     include __DIR__ . '/templates/index.php';
 }
 $page->maincontentarea = ob_get_clean();
-$page->loadSharedcodeFiles();
-if ($cas_client->isLoggedIn()) {
-    $page->navlinks = str_replace('<!-- sublinks -->',
-                                  '<ul>
-                                    <li><a href="./?manage">Manage URLs</a></li>
-                                    <li><a href="./?logout">Logout</a></li>
-                                  </ul>',
-                                  $page->navlinks);
-    
-}
 
 echo $page;
