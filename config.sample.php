ef2a21d8f82342d63ab2d8efb2de8d30517942e0
Kevin Abel <kabel2@unl.edu>
2016-01-07 20:19:32 -0600

    Update sample config/htaccess for new layout

--- config.sample.php
+++ config.sample.php
@@ -1,10 +1,9 @@
-<?php /* conf.inc.php ( config file ) */
+<?php
 
-// Set the include path
-set_include_path(__DIR__ . '/vendor/php');
+require __DIR__ . '/vendor/autoload.php';
 
-// page title
-define('PAGE_TITLE', 'GO URL Generator');
+// Path to system trusted certificates
+define('CAS_CA_FILE', '/etc/pki/tls/cert.pem');
 
 // MySQL connection info
 define('MYSQL_USER', 'myuser');
@@ -12,14 +11,8 @@ define('MYSQL_PASS', 'mypass');
 define('MYSQL_DB',   'goURL');
 define('MYSQL_HOST', 'localhost');
 
-// MySQL tables
-define('URL_TABLE', 'tblURLs');
-
-// use mod_rewrite?
-define('REWRITE', true);
-
 // allow urls that begin with these strings
-$allowed_protocols = array('http://', 'https://', 'mailto:');
+$allowed_protocols = array('http://', 'https://');
 
 // allow urls from these domains for non-authenticated users.
 $allowed_domains = array(
@@ -27,9 +20,6 @@ $allowed_domains = array(
     'nebraska.edu',
     'huskers.com',
     'huskeralum.org',
-    'www.farrp.org',
-    'ceen.unomaha.edu',
-    'throughtheeyes.org',
     'buros.org',
 );
 
@@ -37,4 +27,4 @@ $allowed_domains = array(
 // $allowed_procotols = array();
 
 // uncomment the line below to skip the domain check
-$allowed_domains = array();
+// $allowed_domains = array();
