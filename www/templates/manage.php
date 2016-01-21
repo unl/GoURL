504bc3dd3d1dd6e7c9091eab933baadeb87e8dc5
Kevin Abel <kabel2@unl.edu>
2016-01-20 18:05:03 -0600

    Use responsive table markup in link manager

--- www/templates/manage.php
+++ www/templates/manage.php
@@ -11,7 +11,7 @@
     <div class="wdn-inner-wrapper">
         <?php $urls = $lilurl->getUserURLs(phpCAS::getUser()); ?>
         <?php if ($urls->columnCount()): ?>
-            <table class="go-urls" data-order="[[ 2, &quot;desc&quot; ]]">
+            <table class="go-urls wdn_responsive_table flush-left" data-order="[[ 2, &quot;desc&quot; ]]">
                 <thead>
                     <tr>
                         <th>Short URL</th>
@@ -29,15 +29,15 @@
                     }
                     ?>
                     <tr>
-                        <td><a href="<?php echo $lilurl->getBaseUrl($row['urlID']); ?>"><?php echo $row['urlID']; ?></a></td>
-                        <td><a href="<?php echo $escape($row['longURL']) ?>"><?php echo $escape($row['longURL']) ?></a></td>
-                        <td<?php if ($rowDateTime): ?> data-search="<?php echo $rowDateTime->format('M j, Y m/d/Y') ?>" data-order="<?php echo $rowDateTime->format('U') ?>"<?php endif; ?>>
+                        <td data-header="Short URL"><a href="<?php echo $lilurl->getBaseUrl($row['urlID']); ?>"><?php echo $row['urlID']; ?></a></td>
+                        <td data-header="Long URL"><a href="<?php echo $escape($row['longURL']) ?>"><?php echo $escape($row['longURL']) ?></a></td>
+                        <td data-header="Created on"<?php if ($rowDateTime): ?> data-search="<?php echo $rowDateTime->format('M j, Y m/d/Y') ?>" data-order="<?php echo $rowDateTime->format('U') ?>"<?php endif; ?>>
                             <?php if ($rowDateTime): ?>
                                 <?php echo $rowDateTime->format('M j, Y') ?>
                             <?php endif; ?>
                         </td>
                         <td class="actions">
-                            <a class="wdn-button go-url-qr" href="<?php echo $lilurl->getBaseUrl($row['urlID'] . '.qr') ?>" title="Show QR Code for <?php echo $row['urlID']; ?> Go URL"><span class="qrImage"></span> QR Code®</a>
+                            <a class="wdn-button go-url-qr" href="<?php echo $lilurl->getBaseUrl($row['urlID'] . '.qr') ?>" title="QR Code for <?php echo $row['urlID']; ?> Go URL"><span class="qrImage"></span> QR Code®</a>
                             <form action="<?php echo $lilurl->getBaseUrl('a/links') ?>" method="post">
                                 <input type="hidden" name="urlID" value="<?php echo $row['urlID']; ?>" />
                                 <button type="submit" onclick="return confirm('Are you for sure?');">Delete</button>
