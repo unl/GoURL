441982cbe85aa23a48328a35b41e3641e5ee71b9
Tyler Lemburg <tyler.lemburg@firespring.com>
2017-06-27 15:21:08 -0500

    Update local-footer.php
    Eliminate wild s

--- www/templates/static/local-footer.php
+++ www/templates/static/local-footer.php
@@ -12,7 +12,7 @@
             <ul class="wdn-related-links-v1">
                 <li><a href="http://wdn.unl.edu/">Web Developer Network</a></li>
                 <li><a href="http://iim.unl.edu/">Internet and Interactive Media</a></li>
-                <li><a href="http://ucomm.unl.edu/">University Communications</a></li>
+                <li><a href="http://ucomm.unl.edu/">University Communication</a></li>
                 <li><a href="http://its.unl.edu/">Information Technology Services</a></li>
             </ul>
         </div>
