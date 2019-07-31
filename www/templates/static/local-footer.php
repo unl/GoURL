c3f4cb7f43b0bac344439cd7761a9799f3575144
Jeffrey Sturek <jsturek8@unl.edu>
2019-07-31 11:28:52 -0500

    Replace IIM references with DXG

--- www/templates/static/local-footer.php
+++ www/templates/static/local-footer.php
@@ -2,15 +2,15 @@
   <div class="">
     <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About Go URL</h3>
     <p>Go URL is a URL shortening service similar to <a href="https://bitly.com/" class="external">Bitly</a>. Use this when you would like a shorter URL while retaining the unl.edu domain.</p>
-    <p>This application is a product of the <a href="https://iim.unl.edu/">Internet and Interactive Media group at Nebraska</a>. IIM is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
+    <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group at Nebraska</a>. DXG is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
   </div>
 </nav>
 <nav id="dcf-footer-group-2" role="navigation" aria-labelledby="dcf-footer-group-2-heading">
   <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-2-heading">Related Links</h3>
   <ul class="dcf-list-bare dcf-mb-0">
-    <li><a href="http://wdn.unl.edu/">Web Developer Network</a></li>
-    <li><a href="http://iim.unl.edu/">Internet and Interactive Media</a></li>
-    <li><a href="http://ucomm.unl.edu/">University Communication</a></li>
-    <li><a href="http://its.unl.edu/">Information Technology Services</a></li>
+    <li><a href="https://wdn.unl.edu/">Web Developer Network</a></li>
+    <li><a href="https://dxg.unl.edu/">Digital Experience Group</a></li>
+    <li><a href="https://ucomm.unl.edu/">University Communication</a></li>
+    <li><a href="https://its.unl.edu/">Information Technology Services</a></li>
   </ul>
 </nav>
