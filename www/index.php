f06a69a7379a943673d841c4b9650111f66c22bd
Peter Nguyen <petervnguyen99@gmail.com>
2019-11-05 11:37:03 -0600

    Set no Cas server validation due to scripting error.

--- www/index.php
+++ www/index.php
@@ -16,7 +16,8 @@ session_name('gourl');
 $route = '';
 $pathInfo = $lilurl->getRequestPath();
 phpCAS::client(CAS_VERSION_2_0, 'shib.unl.edu', 443, '/idp/profile/cas');
-phpCAS::setCasServerCACert(CAS_CA_FILE);
+//phpCAS::setCasServerCACert(CAS_CA_FILE);
+phpCAS::setNoCasServerValidation();
 phpCAS::handleLogoutRequests();
 
 function sendCORSHeaders() {
