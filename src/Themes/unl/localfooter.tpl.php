995583231e8ec3c0842b30facfb17c29a887b17c
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-08-18 14:57:10 -0500

    Updated footer text (#99)

--- src/Themes/unl/localfooter.tpl.php
+++ src/Themes/unl/localfooter.tpl.php
@@ -2,7 +2,11 @@
     <div class="">
         <h3 class="dcf-txt-md dcf-bold dcf-uppercase dcf-lh-3 unl-ls-2 unl-cream" id="dcf-footer-group-1-heading">About Go URL</h3>
         <p>Go URL is a URL shortening service similar to <a href="https://bitly.com/" class="external">Bitly</a>. Use this when you would like a shorter URL while retaining the unl.edu domain.</p>
-        <p>This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group at Nebraska</a>. DXG is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
+        <p>
+            This application is a product of the <a href="https://dxg.unl.edu/">Digital Experience Group</a>
+            at <a href="https://www.unl.edu/">Nebraska</a>. DXG is a part
+            of <a href="https://ucomm.unl.edu/">University
+            Communication &amp; Marketing</a>.</p>
     </div>
 </nav>
 <nav id="dcf-footer-group-2" role="navigation" aria-labelledby="dcf-footer-group-2-heading">
